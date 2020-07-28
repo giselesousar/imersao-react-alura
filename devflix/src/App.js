9a9678d3f3ee84953649a0f74d8f4479e70982ef
Gisele <giselesousar@gmail.com>
2020-07-27 22:01:44 -0300

    initial commit

---
+++ devflix/src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import Menu from './components/Menu';
+import BannerMain from './components/BannerMain';
+import Carousel from './components/Carousel';
+import Footer from './components/Footer';
+import dadosIniciais from './data/dados_iniciais.json';
+
+function App(){
+    return(
+        <div>
+            <Menu />
+            {/**<BannerMain 
+                videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
+                url={dadosIniciais.categorias[0].url}
+                videoDescription={"Font-end"}
+            />
+            <Carousel 
+                ignoreFirstVideo
+                category={dadosIniciais.categorias[0]}
+            />*/}
+            <Footer />
+        </div>
+    )
+}
+
+export default App;
